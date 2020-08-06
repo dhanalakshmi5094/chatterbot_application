6a0b11cfb53c66de934b03a5dbd8161796a10bf7
mohanrao487 <mohanamarama.aucse@gmail.com>
2020-08-06 15:43:28 +0530

    basic_example of chatterbot application.

---
+++ basic_example.py
@@ -0,0 +1,20 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# Create a new chat bot named Charlie
+chatbot = ChatBot('Charlie')
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+
+    "Hi, can I help you?",
+    "Sure, I'd like to book a flight to Iceland.",
+    "Your flight has been booked."
+])
+while True:
+    request = input('You')# Get a response to the input text 'I would like to book a flight.'
+    response = chatbot.get_response(request)
+    print('Bot:', response)
+print(response)
+
